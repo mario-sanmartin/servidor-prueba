568277860fb00761a64dfd4a79f782291006b056
mario-sanmartin <sanmartin.mario@outlook.com>
2020-11-01 23:14:57 -0300

    Promesillas y recursividad fail

--- public/main.js
+++ public/main.js
@@ -1,3 +1,5 @@
+// const axios = require('axios')
+
 const numeros = [1,2,3,4,5,6,7,8,9,]
 
 const mascotas = [
@@ -47,7 +49,11 @@ console.log('edades mascostas',edades);
 const resultado = suma(edades);
 console.log(resultado/ edades.length);
 
+///FIN MAP
 
+
+
+//************************************************/
 //REDUCE
 // const reducer = (acumulador, valorActual) => nuevoAcumulador
 
@@ -73,3 +79,79 @@ const plano = anidado.reduce((acc,el)=> acc.concat(el),[])
 //acc es un arreglo vacio y el primer elemento es uno
 
 console.log(plano);
+//FIN REDUCE
+/************************************************ */
+
+//RECURSIVIDAD NAZI
+//una funcion que se llama asi misma hasta que
+//que se cumple una condicion de salida
+//Eliminacion de ciclos for anidados
+// const conteoRegresivo = (a) => {
+//     if(a < 0) return
+//     console.log(a)
+//     return conteoRegresivo(a - 1)
+// }
+
+// conteoRegresivo(10)
+
+// const llamadoAPA = async(url,llamados = 0) =>{
+//     try {
+//         const result = await axios.get(url)
+//         console.log(result)
+//         return result
+//     } catch (error) {
+//         if(llamados > 5){
+//             return ''
+//         }
+//         console.log(e);
+//         return llamadoAPA(url,llamados + 1)
+//     }
+// }
+// llamadoAPA('https://jsonplaceholder.typicode.com/users')
+
+
+
+
+
+
+
+
+
+
+
+
+//*********************/
+//PROMESAS
+const p1 = Promise.resolve(1)
+console.log(p1);
+
+//si queremos acceder al valor de una promesa
+//debemos usar .then()
+// p1
+// .then(x => console.log(x));
+
+// p1
+//   .then(x => x +5)
+// //   .then(x => console.log(x)); vale 6
+//     .then(x => Promise.resolve(x + 5))
+//     .then(x => console.log(x))
+
+//Especialmente para llamadas a bd
+const retrazo = x => new Promise((resolve,reject)=> setTimeout(()=> resolve(x),1000))
+
+retrazo(7)
+    // .then(x => console.log('Promesa retrazada', x))
+    // .then(x => retrazo(7 + 7))
+    .then(x => {
+        console.log(x)
+        return retrazo (x + 7)
+    })
+    .then(x => console.log(x))
+    .then(x => Promise.reject('Ocurrio un error :('))
+    .catch(e => console.log(e))
+
+
+    //fetch Api
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(x => x.json()) //primero se pasa a json
+        .then(x => console.log(x)) //luego se muestra
